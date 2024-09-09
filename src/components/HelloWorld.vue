abc1672efd826f765c7f292d9a3b61bcf3858108
khang <khang.h.t@utexas.edu>
2024-09-09 02:15:18 -0500

    Start fresh with vue and vite

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Khang Tran</h1>
+  <h2>
+    This site is a work in progress 
+    <br>
+    Check in later for updates!
+  </h2>
+</template>
+
+<style scoped>
+</style>
