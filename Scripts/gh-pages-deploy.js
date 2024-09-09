597354f6b40afd3098c81436a1bc5d0a1877f4cd
khang <khang.h.t@utexas.edu>
2024-09-09 03:02:04 -0500

    Change deployment branch to portfolio

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -4,16 +4,16 @@ import { existsSync } from "fs";
 import { platform } from "os";
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "portfolio"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "portfolio"]);
+    console.log("Pushing to portfolio...");
+    await execa("git", ["push", "origin", "HEAD:portfolio", "--force"]);
 
     if (platform() === "win32") {
         await execa("cmd", ["/c", "del", "/s", "/q", folderName]);
@@ -24,7 +24,7 @@ import { platform } from "os";
       }
   
     await execa("git", ["checkout", "-f", "main"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
+    await execa("git", ["branch", "-D", "portfolio"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
