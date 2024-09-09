ace610882e6f7c91c4ab4d9fe39e3acb5da75066
khang <khang.h.t@utexas.edu>
2024-09-09 03:00:15 -0500

    should be good now

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { execa } from "execa";
 import { existsSync } from "fs";
+import { platform } from "os";
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
@@ -13,8 +14,16 @@ import { existsSync } from "fs";
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rmdir", [folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
+
+    if (platform() === "win32") {
+        await execa("cmd", ["/c", "del", "/s", "/q", folderName]);
+        await execa("cmd", ["/c", "rmdir", "/s", "/q", folderName]);
+      } else {
+        // Assuming Unix-based system
+        await execa("rm", ["-rf", folderName]);
+      }
+  
+    await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
